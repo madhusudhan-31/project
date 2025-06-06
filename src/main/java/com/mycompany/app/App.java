b9ffd4fe2dbba06887ae7014b7ffde19abb7a88e
madhusudhan-31 <madhusudhangowdahg31@gmail.com>
2025-06-06 09:02:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,3 +8,61 @@ public class App {
         System.out.println("Hello World!");
     }
 }
+/**
+pipeline {
+
+agent any
+
+stages {
+
+stage('Checkout') {
+
+steps {
+
+git branch: 'main', url:
+'https://github.com/azadCMRIT/hello-maven.git' // Replace with
+your repository URL
+
+}
+
+}
+
+stage('Build') {
+
+steps {
+
+// Use 'mvn clean package' for Maven or 'gradle
+build' for Gradle
+
+sh 'mvn clean package'
+
+}
+
+}
+
+stage('Test') {
+
+steps {
+
+// Run unit tests using Maven or Gradle
+
+sh 'mvn test'
+
+}
+
+}
+
+// Add additional stages for deployment, code analysis, etc.
+as needed
+
+}
+
+}
+ git config --global push.autoSetupRemote 
+true
+ git push origin main
+  git remote add origin 
+git@github.com:cmritsarada/exp6
+ ssh-keygen -t ed25519 -C "your_email@example.com"
+
+*/
